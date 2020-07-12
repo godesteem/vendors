33c1232a9ac3d61cc3d314b1e5993e807c3f05ea
Philipp Zettl <philipp@godesteem.de>
2020-07-12 10:56:46 +0200

    drop precompiled header

--- stb_image/stb_image.cpp
+++ stb_image/stb_image.cpp
@@ -5,7 +5,5 @@
  * Last Modified Date: 25.02.2020
  * Last Modified By  : Philipp Zettl <philipp.zettl@godesteem.de>
  */
-#include "bepch.h"
-
 #define STB_IMAGE_IMPLEMENTATION
 #include "stb_image.h"
